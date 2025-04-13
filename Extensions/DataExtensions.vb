d35f3b834d563312f7d2226f30f15b88599e56ca
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-04-13 19:52:59 +0200

    Dokumentation der IsDivisibleBy-Funktion aktualisiert
    Die Dokumentation wurde präzisiert, um klarzustellen, dass die Funktion überprüft, ob ein Wert ohne Rest durch einen Divisor teilbar ist. Außerdem wurden die Parameterbeschreibungen für die Überladungen der Funktion angepasst, um Konsistenz zu gewährleisten und kleinere Schreibfehler zu korrigieren (z.B. "Devisor" statt "Divisor").

--- Extensions/DataExtensions.vb
+++ Extensions/DataExtensions.vb
@@ -877,7 +877,7 @@ Public Module DataExtensions
 #Region "Bestimmen ob ein Wert durch einen angegebenen Wert teilbar ist"
 
   ''' <summary>
-  ''' Bestimmt, Wert durch den angegebenen Wert teilbar ist (ohne Rest).
+  ''' Bestimmt, ob der Wert durch den angegebenen Wert teilbar ist (ohne Rest).
   ''' </summary>
   ''' <param name="Divisor">
   ''' Der Wert, durch den dividiert werden soll.
@@ -887,73 +887,105 @@ Public Module DataExtensions
   ''' </returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Short, Divisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um zu überprüfen, ob der Wert ohne Rest durch den Divisor teilbar ist.
+    ' Die interne Funktion führt die eigentliche Berechnung durch und gibt das Ergebnis zurück.
     Return InternalIsDivisibleBy(sender, Divisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene UShort-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende UShort-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As UShort, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene Integer-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende Integer-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Integer, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene UInteger-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende UInteger-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As UInteger, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene Long-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende Long-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Long, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene ULong-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende ULong-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As ULong, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene Decimal-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende Decimal-Wert.</param>
+  ''' <param name="Dvisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Decimal, Dvisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Dvisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene Single-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende Single-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Single, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
-  ''' <inheritdoc cref="IsDivisibleBy(Short, Double)"/>
+  ''' <summary>
+  ''' Überprüft, ob der übergebene Double-Wert ohne Rest durch den angegebenen Divisor teilbar ist.
+  ''' </summary>
+  ''' <param name="sender">Der zu überprüfende Double-Wert.</param>
+  ''' <param name="Devisor">Der Divisor, durch den geteilt werden soll.</param>
+  ''' <returns>Gibt <see langword="True"/> zurück, wenn der Wert teilbar ist, andernfalls <see langword="False"/>.</returns>
   <DebuggerStepThrough> <Extension>
   Public Function IsDivisibleBy(sender As Double, Devisor As Double) As Boolean
-
+    ' Ruft die interne Funktion auf, um die Teilbarkeit zu überprüfen.
     Return InternalIsDivisibleBy(sender, Devisor)
-
   End Function
 
 #End Region
