2fd18d4bfa8624601a126960e86fd3758537ff80
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-04-27 20:29:16 +0200

    Neues Benutzersteuerelement IsMultipleOfControl hinzugefügt
    Die Datei `IsMultipleOfControl.Designer.vb` enthält jetzt die
    Definition für das Benutzersteuerelement `IsMultipleOfControl`,
    das eine konfigurierbare `TextBox` umfasst.
    
    Zusätzlich wurde in `IsMultipleOfControl.resx` ein neues
    Ressourcenformat im XML-Format implementiert, das die
    Struktur und Metadaten für die Anwendung beschreibt.
    
    In `IsMultipleOfControl.vb` wurde die Klasse
    `IsMultipleOfControl` mit einer leeren Methode `Start`
    hinzugefügt, die als Logik-Backend für das Steuerelement
    dienen kann.

---
+++ ExtensinsDemo/IsMultipleOfControl.vb
@@ -0,0 +1,23 @@
+﻿' *************************************************************************************************
+' 
+' IsMultipleOfControl.vb
+' Copyright (c)2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' *************************************************************************************************
+
+Imports SchlumpfSoft.Extensions
+
+Public Class IsMultipleOfControl
+
+  Public Sub Start()
+
+
+
+
+
+
+  End Sub
+
+End Class
