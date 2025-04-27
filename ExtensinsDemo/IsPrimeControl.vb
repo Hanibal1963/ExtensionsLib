45b9ed35a02d7ed34a5c61a902822a39cb60e90d
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-04-27 19:27:24 +0200

    Füge IsPrimeControl Benutzersteuerelement hinzu
    Implementiere die IsPrimeControl-Klasse mit einer
    TextBox, die mehrzeilig, nur lesbar und mit
    Scrollleisten ausgestattet ist. Aktualisiere die
    Ressourcendatei für das XML-Schema und füge eine
    Start-Methode hinzu, um die Schriftart der
    TextBox auf "Courier New" zu setzen.
    Verbessert die Benutzeroberfläche und Funktionalität.

---
+++ ExtensinsDemo/IsPrimeControl.vb
@@ -0,0 +1,25 @@
+﻿' *************************************************************************************************
+' 
+' IsPrimeControl.vb
+' Copyright (c)2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' *************************************************************************************************
+
+Imports SchlumpfSoft.Extensions
+
+Public Class IsPrimeControl
+
+  Public Sub Start()
+
+    ' Monospace-Schriftart für die TextBox setzen
+    Me.TextBox1.Font = New Font("Courier New", 10)
+
+
+
+
+
+  End Sub
+
+End Class
