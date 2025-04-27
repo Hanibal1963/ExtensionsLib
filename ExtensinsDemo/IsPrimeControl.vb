9ce495209db76215afd6274af5585c07e1fce644
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-04-27 19:49:51 +0200

    Verbessere IsPrimeControl: TextBox-Formatierung und Logik
    Ändere die Methode `Start`, um eine Monospace-Schriftart zu setzen, einen Infotext zu laden und eine Kopfzeile für die Primzahlüberprüfung zu erstellen. Füge eine Schleife hinzu, die die Zahlen von 1 bis 100 überprüft und die Ergebnisse in der TextBox anzeigt. Optimiere die Zeilenformatierung für eine bessere Lesbarkeit.

--- ExtensinsDemo/IsPrimeControl.vb
+++ ExtensinsDemo/IsPrimeControl.vb
@@ -16,9 +16,24 @@ Public Class IsPrimeControl
     ' Monospace-Schriftart für die TextBox setzen
     Me.TextBox1.Font = New Font("Courier New", 10)
 
+    ' Infotext laden
+    Me.TextBox1.Text = My.Resources.IsPrime_InfoText
 
+    ' Kopfzeile erstellen
+    Dim header As String = $"{"Wert",-5}| {"ist eine Primzahl",-15}{vbCrLf}"
+    Me.TextBox1.Text &= header
+    Me.TextBox1.Text &= New String("-"c, header.Length) & vbCrLf
 
+    Dim line As String
+    For i As Integer = 1 To 100
 
+      ' Zeile formatieren
+      line = $"{i,-5}| {i.IsPrime,-15}{vbCrLf}"
+
+      ' Zeile anhängen
+      Me.TextBox1.Text &= line
+
+    Next
 
   End Sub
 
