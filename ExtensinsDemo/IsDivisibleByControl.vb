2ceabfa7231e30381e2680d84d085e7b7b56c361
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-04-27 20:28:54 +0200

    Neue Benutzersteuerung IsDivisibleByControl hinzugefügt
    In dieser Änderung wurde die Benutzersteuerung `IsDivisibleByControl` implementiert, die eine konfigurierbare TextBox enthält. Die TextBox ist mehrzeilig, nur lesbar und passt sich der Größe des Containers an.
    
    Zusätzlich wurde eine neue Ressourcendatei im XML-Format erstellt, die die benötigten Metadaten und die Struktur für die Benutzeroberfläche definiert.
    
    Die Klasse `IsDivisibleByControl` enthält eine leere Methode `Start`, die für zukünftige Implementierungen vorgesehen ist.

---
+++ ExtensinsDemo/IsDivisibleByControl.vb
@@ -0,0 +1,22 @@
+﻿' *************************************************************************************************
+' 
+' IsDivisibleByControl.vb
+' Copyright (c)2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' *************************************************************************************************
+
+Imports SchlumpfSoft.Extensions
+
+Public Class IsDivisibleByControl
+
+  Public Sub Start()
+
+
+
+
+
+  End Sub
+
+End Class
