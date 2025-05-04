3f6a78009964a49ff7c98770a3c34dbbaba480f8
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-04 17:46:36 +0200

    Neues Benutzersteuerelement für hexadezimale Konversion
    Fügt die Klasse `HexadecimalConversionControl` hinzu, die von `System.Windows.Forms.UserControl` erbt.
    Die Designer-Datei konfiguriert eine mehrzeilige, schreibgeschützte `TextBox`.
    Die Hauptdatei enthält die Methode `Start`, die die Schriftart setzt, einen Infotext lädt und eine Kopfzeile erstellt.
    Zusätzlich wurde das XML-Schema in der Ressourcen-Datei definiert.

---
+++ ExtensinsDemo/HexadecimalConversionControl.vb
@@ -0,0 +1,33 @@
+﻿' *************************************************************************************************
+' 
+' HexadecimalConversionControl.vb
+' Copyright (c)2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' *************************************************************************************************
+
+Imports SchlumpfSoft.Extensions
+
+Public Class HexadecimalConversionControl
+  Public Sub Start()
+
+    ' Monospace-Schriftart für die TextBox setzen
+    Me.TextBox1.Font = New Font("Courier New", 10)
+
+    ' Infotext laden
+    Me.TextBox1.Text = My.Resources.HexadecimalConversion_InfoText
+
+    'TODO: Code für die Demo noch erstellen
+    Me.TextBox1.Text &= $"Code für die Demo muß noch erstellt werden !" & vbCrLf & vbCrLf
+
+
+    ' Kopfzeile erstellen
+    Dim header As String = $"{"Wert",-5}| {"",-15}{vbCrLf}"
+    Me.TextBox1.Text &= header
+    Me.TextBox1.Text &= New String("-"c, header.Length) & vbCrLf
+
+
+  End Sub
+
+End Class
