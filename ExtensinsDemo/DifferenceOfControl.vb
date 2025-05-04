093e14bb3d471cae0382e6a1c1ec47e81d9a711b
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-04 17:11:25 +0200

    Neues Benutzersteuerelement 'DifferenceOfControl' hinzugefügt
    Diese Änderungen umfassen die Implementierung einer neuen Klasse `DifferenceOfControl`, die von `System.Windows.Forms.UserControl` erbt. In der Designer-Datei wurden die Komponenten, einschließlich einer mehrzeiligen, nur lesbaren `TextBox`, definiert. Das XML-Schema in der Ressourcen-Datei wurde aktualisiert, um die Struktur und Metadaten zu reflektieren. Zudem wurde in der Hauptdatei eine Methode `Start` hinzugefügt, die die Schriftart der `TextBox` auf "Courier New" setzt, einen Infotext lädt und eine Kopfzeile erstellt. Diese Änderungen verbessern die Benutzeroberfläche und Funktionalität des Steuerelements.

---
+++ ExtensinsDemo/DifferenceOfControl.vb
@@ -0,0 +1,29 @@
+﻿' *************************************************************************************************
+' 
+' DifferenceOfControl.vb
+' Copyright (c)2025 by Andreas Sauer 
+'
+' Kurzbeschreibung:
+' 
+' *************************************************************************************************
+
+Imports SchlumpfSoft.Extensions
+
+Public Class DifferenceOfControl
+
+  Public Sub Start()
+
+    ' Monospace-Schriftart für die TextBox setzen
+    Me.TextBox1.Font = New Font("Courier New", 10)
+
+    ' Infotext laden
+    Me.TextBox1.Text = My.Resources.DifferenceOf_InfoText
+
+    ' Kopfzeile erstellen
+    Dim header As String = $"{"Wert",-5}| {"",-15}{vbCrLf}"
+    Me.TextBox1.Text &= header
+    Me.TextBox1.Text &= New String("-"c, header.Length) & vbCrLf
+
+  End Sub
+
+End Class
