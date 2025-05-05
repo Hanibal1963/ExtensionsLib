7d5c6c810ec1ebc14108839a63ead9ee8a6bc12f
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-05 18:49:48 +0200

    Aktualisiere IsInRangeOfControl für Demo-Anzeige
    Die Methode `IsInRangeOfControl` wurde angepasst, um den Text in `TextBox1` zu aktualisieren. Der Kommentar "TODO: Code für die Demo noch erstellen" wurde entfernt und durch eine Kopfzeile ersetzt, die anzeigt, dass der Wert zwischen 40 und 60 liegt. Eine Schleife wurde hinzugefügt, die Werte von 5 bis 100 in Schritten von 5 überprüft und die Ergebnisse formatiert an den Text in `TextBox1` anhängt.

--- ExtensinsDemo/IsInRangeOfControl.vb
+++ ExtensinsDemo/IsInRangeOfControl.vb
@@ -19,13 +19,21 @@ Public Class IsInRangeOfControl
     ' Infotext laden
     Me.TextBox1.Text = My.Resources.IsInRangeOf_InfoText
 
-    'TODO: Code für die Demo noch erstellen
-    Me.TextBox1.Text &= $"Code für die Demo muß noch erstellt werden !" & vbCrLf & vbCrLf
-
     ' Kopfzeile erstellen
-    Dim header As String = $"{"Wert",-5}| {"",-15}{vbCrLf}"
+    Dim header As String = $"{"Wert",-5}| {"Der Wert liegt zwischen 40 und 60",-15}{vbCrLf}"
     Me.TextBox1.Text &= header
     Me.TextBox1.Text &= New String("-"c, header.Length) & vbCrLf
+
+    Dim line As String
+    For i As Double = 5 To 100 Step 5
+
+      ' Zeile formatieren
+      line = $"{i,-5}| {i.IsInRangeOf(40, 60),-15}{vbCrLf}"
+
+      ' Zeile anhängen
+      Me.TextBox1.Text &= line
+
+    Next
 
   End Sub
 
